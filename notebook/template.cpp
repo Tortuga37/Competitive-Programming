5983b22f3ad96423f183aa546dcc47151dffc3bf
Tortuga-X <juanandresbetancur2006@gmail.com>
2026-04-20 12:13:22 -0500

    Template

---
+++ notebook/template.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fi first
+#define se second
+#define lli long long int
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define lb lower_bound
+#define ub upper_bound
+#define deb(x) cout << #x << " = " << x << '\n';
+using namespace std;
+typedef long double ld;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<lli> vl;
+typedef vector<vector<lli>> vvl;
+typedef vector<ii> vii;
+typedef vector<string> vs;
+
+void read(auto &x){
+    for(auto &e:x) cin >> e;
+}
+
+/*#include<ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using indexed_set = tree<ii, null_type, less<ii>, rb_tree_tag, tree_order_statistics_node_update>;
+using indexed_set = tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>;*/
+
+void solve(){
+    
+}
+
+int main() {
+    fast
+    int t=1;
+    //cin>>t;
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
